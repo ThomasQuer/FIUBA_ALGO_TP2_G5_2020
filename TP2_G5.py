e78749951122d266bc023a73233d747619904977
constanzasalvo <csalvo@fi.uba.ar>
2021-02-10 17:42:21 -0300

    Función seleccion_token correcta, se modificaron dar_like_posteo, leer_posteo y subir_posteo, listar_amigos por agregar la misma.

--- TP2_G5.py
+++ TP2_G5.py
@@ -1,9 +1,8 @@
 import json
 import facebook
 import requests
-import fbchat
-import re
-
+#import fbchat
+#import re
 def obtener_nombre_usuario(token):
     """
     PRE:
@@ -26,7 +25,7 @@ def buscar_usuario(token, nombre_usuario):
     
     return usuario_ID
 
-def dar_like_posteo(token, id_posteo):
+def dar_like_posteo():
     """
     PRE:
         token debe ser un string, la llave de acceso 
@@ -34,9 +33,12 @@ def dar_like_posteo(token, id_posteo):
     POST:
         genera un like en el posteo enviado
     """
-    #con token de pagina desde app empresarial
-    ##ej: putlike = graph.put_like(object_id = id_posteo) # id_posteo = USERID_POSTID
-    darlike = graph.put_like(object_id ="105249781540470_106764151389033")
+    #Se selecciona token de pagina desde una app empresarial y se utiliza api
+    token, graph = seleccion_token("empresarial_pagina")
+    #Se solicita id del posteo a dar like
+    id_posteo = input("Ingrese el id del posteo utilizando la forma 'IDUSUARIO_IDPOSTEO': ") #ej: 105249781540470_106764151389033
+    #Se utiliza la api para dar like al posteo y se imprime por pantalla el resultado
+    darlike = graph.put_like(object_id = id_posteo)
     if darlike:
         print("Se ha dado like al posteo.")
     else:
@@ -45,27 +47,28 @@ def dar_like_posteo(token, id_posteo):
 def leer_posteo(id_usuario):
     """
     PRE:
-       id_usuario debe ser un string con el id del usuario
+       
     POST:
         Solicita id del posteo e imprime el mensaje del posteo seleccionado
     """
-    id_posteo = input('Ingrese el id del posteo: ')
-    identificador = str(str(id_usuario) + '_' + id_posteo)
-    #token utilizado el de app comercial #id = 'USERID_POSTID' #ej: identificador = 101662381858155_101493071875086
+    token, graph = seleccion_token("consumidor_cuenta")
+    id_usuario = input('Ingrese el id del usuario: ') # 101662381858155
+    id_posteo = input('Ingrese el id del posteo: ') #ej 101493071875086
+    identificador = str(id_usuario + '_' + id_posteo)
     posteo = graph.get_object(id = identificador, fields ='message, attachments{description}') 
     print(posteo['message'])
     
-def subir_posteo(id_pagina):
+def subir_posteo():
     """
     PRE:
-        id pagina debe ser un string
     POST:
         permite escribir un texto y lo publica en una pagina
     """
-    #con token de pagina desde app empresarial
+    #Se selecciona token de pagina desde una app empresarial y se utiliza api
+    token, graph = seleccion_token("empresarial_pagina")
     mensaje = input('Ingrese el mensaje del posteo: ')
-    #posteo = graph.put_object(parent_object = id_pagina, connection_name ="feed", message = mensaje)
-    posteo = graph.put_object(parent_object ="105249781540470", connection_name ="feed", message = mensaje)
+    id_pagina = input('Ingrese el id de la página: ') #ej "105249781540470"
+    posteo = graph.put_object(parent_object = id_pagina, connection_name ="feed", message = mensaje)
     if posteo:
         print("Su posteo número: " +  posteo['id'] + " ha sido publicado.")
     else:
@@ -102,13 +105,12 @@ def listar_seguidores(token):
     """
     return lista_seguidores
 
-def listar_amigos(token):
+def listar_amigos():
     """
-    PRE:
-        token debe ser un string, la llave de acceso 
-    POST:
-        Devuelve una lista con los amigos
+    PRE: necesita funcion seleccion_token()
+    POST: Devuelve una lista con los amigos.
     """
+    token, graph = seleccion_token("consumidor_cuenta")
     amigos = graph.get_object('me', fields='friends')
     #Excepcion: Solo los amigos que han instalado esta aplicación estarán en la versión 2.0 o superior de la API.
     print("La cantidad de amigos que tienes es: " + str(amigos['friends']['summary']['total_count']))
@@ -133,12 +135,13 @@ def solicitar_amistad(token, usuario_id):
     """
     return
 
+"""#momentaneamente desactivado
 def enviar_mensaje_usuario(nombre_usuario):
     """
-    PRE:
-        nombre_usuario debe ser un string que indique el nombre de usuario de la cuenta de facebook.
-    POST:
-       envia un mensaje a usuario posteriormente ingresado.
+   # PRE:
+    #    nombre_usuario debe ser un string que indique el nombre de usuario de la cuenta de facebook.
+    #POST:
+     #  envia un mensaje a usuario posteriormente ingresado.
     """
     #seteo de variables para fbchat.Client (sino genera un error)
     fbchat._util.USER_AGENTS    = ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"]
@@ -156,6 +159,7 @@ def enviar_mensaje_usuario(nombre_usuario):
         print("Se ha enviado correctamente el mensaje.") 
     else:
         print("Hubo un problema, intente nuevamente.")
+"""
 
 def actualizar_datos_perfil(token):
     """
@@ -177,19 +181,59 @@ def ver_ultimos_posts(token):
     lista_de_posts_json = lista_de_posts.json()
     print(lista_de_posts_json)
 
-
-def main():
-    #token: ver de hacer funcion para seleccion de empresarial o consumidor o pagina..
-    #empreserial
-    token = 'EAAPQlFICfVYBAD0cZCI5zrnjJNxkTGOZB0yvu9yhAE9RTLQJl45hmwNTEDAaKOt0i6bKI3o2Tuay4HVmTutpqO4ZAPn0dcw9O2jGFIClF6mJAgYpKZAnHkqaVoM5cFM8napmVBTpuNm62GaFdnHhWCTZCIeiiOmb5KRNGDshp8VZCisvn7LOryQQArnIR91Y8ZD'
-    #consumidor
-    token = 'EAADBGgWWrIABAPGUyb39PfOjMAUvHK4TZAwx5LG62ZAE4eX1fz0GDDVsC6ZC0Wmsv6vhShLWz81HZCpj6anIVHC8bZC51WZAL0h9Iel6UEsfC2Db9zNYdhhDsNiZAv0pLMCufPkZAy3PorlXdsNwd8TPtHa2d0MeLlKTngCktd2ZB9ADVuKKhQObNtc43UjE3OEWFfu557aqBkkJcS09ex7KT88AODlWryh7dgLY8nLE56gZDZD'
-    #empresaria - Crux bot pagina
-    token = 'EAAPQlFICfVYBAJZAZAFUpvjFUfLCtFjgDGnY9RPU9Cv3F9T5phetuZAAotIXNK9AZCdJu7yfbzxLAd9atNCUjHbNptuWqvhxZBbNqc1TbeUYFFZC1mtiUPS0EKcUGZB5FNIqZBa8tvWxVjPTYJfEiZBuHlDJ6IfGZCYnQDsvdQHnMQ1lFI3171hsr42hVEq0Klvn6pzeoHvQ4MwwZDZD'
-    #Consumidor - Crux Bot Pagina
-    token = 'EAADBGgWWrIABAPAD23afpNqdRQKDEi8L5G0P91aMR2peBGg492WSmDNzGblQ9xLPYN5E9Xvu2PZA03id63y5TmQuI6DZCvZAZBz3OjzzoFCZBASehoCM8jWuXgUfWspXJ1OWwRhoa2uIgayAos0EyfeMf0vP07pEMzwveh1ZCtV6MY9Ph25i4DPHsUYSmESVEZD'
-    
+def seleccion_token(tipo_token):
+    """
+    PRE: necesita un string indicando el tipo de token a devolver. Opciones: empresarial_cuenta, empresarial_pagina, consumidor_cuenta, consumidor_pagina.
+    POST: devuelve un string con el token segun tipo de aplicación y el objeto graph del tipo 'facebook.GraphAPI'.
+    """
+    if tipo_token == "empresarial_cuenta":
+        token = 'EAAPQlFICfVYBAHQTuF4SA84zmZBZCWZAdJH7qIeAvL6JRYY2gZCsIwwhua67QHtVYJFCOpa3sLpN2lwkwddmIqy8ZCfejRaeReWcExZCtDzaGW6ifnWrwXlD1DZAS36T5pyYSRujfLxNcNDZBZBeA9PqZAVOzHGNkeYvAhbSKUvDsbyAZDZD'
+    elif tipo_token == "empresarial_pagina":
+        token = 'EAAPQlFICfVYBAGksuFWsbDomJ5DFFYuL4MQ5gzIKYDXVfsJRzCk9uoNK9hZCcnDSdiDiDz5Y4HFvO1G7r63Jnx1sZA5bj3cT9pQVAeyUdMUaVd6VqX7BPMztN8jYDrqdIN7fIeYc0ZBwEKZB9ZCZCryTZAtmXFrJua76OPJZC8bYLwZDZD'
+    elif tipo_token == "consumidor_cuenta":
+        token = 'EAADBGgWWrIABAOCBW4316mP4J3D5iqZAcEQ48wKBrftnoOFnb452KPO4wdlfpN5MAWu8h3DGyPdqZCLMjUgH9A6nJLZASMnnxQZCHwELkM47biGgc3WJbkXLZAxOMknHblPJLvnZCsgwtfqoagitQaY0gHpRoxDbp2o56xWDZBDNwZDZD'
+    elif tipo_token == "consumidor_pagina":
+        token = 'EAADBGgWWrIABAPAD23afpNqdRQKDEi8L5G0P91aMR2peBGg492WSmDNzGblQ9xLPYN5E9Xvu2PZA03id63y5TmQuI6DZCvZAZBz3OjzzoFCZBASehoCM8jWuXgUfWspXJ1OWwRhoa2uIgayAos0EyfeMf0vP07pEMzwveh1ZCtV6MY9Ph25i4DPHsUYSmESVEZD'
     graph = facebook.GraphAPI(token)
+    return token, graph
+
+#de uso interno
+def generar_token_extendido():
+    """PRE
+    POST: devuelve por pantalla el token extendido."""
+    id_empresarial = "1073760379764054"
+    app_secret_empresarial = "713e642a5b1713edd95a3318fcc53d9c"
+    id_comercial = "212317507071104"
+    app_secret_comercial = "76993c5b1107a3771fc4bf8b7cd977aa"
+    token_corto = input("Ingrese el token de corto período: ") 
+    tipo_app = int(input("Ingrese si 1 si es app empresarial o 2 si es app comercial: "))
+    
+    if tipo_app == 1:
+        id_app = id_empresarial
+        app_secret = app_secret_empresarial
+    elif tipo_app == 2:
+        id_app = id_comercial
+        app_secret = app_secret_comercial
+    
+    token_extendido = requests.get(f"https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id={id_app}&client_secret={app_secret}&fb_exchange_token={token_corto}")
+    print("Token extendido: " + token_extendido.json()['access_token'])
+
+def generar_token_pagina():
+    """ PRE: utiliza función seleccion_token()
+    POST: devuelve por pantalla token de pagina. Si el token de la app en la función seleccion_token() es de larga vida, el token de pagina no caduca"""
+
+    tipo = int(input("Ingrese 1-empresarial_cuenta o 2-consumidor_cuenta: "))
+    if tipo == 1:
+        tipo_token = "empresarial_cuenta"
+    elif tipo == 2:
+        tipo_token = "consumidor_cuenta"
+    token = seleccion_token(tipo_token)
+    token_pagina = requests.get(f"https://graph.facebook.com/v9.0/105249781540470?fields=access_token&access_token={token}")
+    print("El token pagina es: " + token_pagina.json()['access_token'])
+
+#main
+def main():
+
 
 if __name__ == "__main__":
     main()
